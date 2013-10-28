c78011404acb2a468a4d8155718e134d651ebf17
Jack Maloney <jmmaloney4@gmail.com>
2013-10-27 19:57:39 -0500

    Made bell sheped curve generator generate a true bell shaped curve

--- huffmantrees/Rand.java
+++ huffmantrees/Rand.java
@@ -3,7 +3,7 @@ package huffmantrees;
 import java.util.Random;
 
 public class Rand {
-	static int[] BellShapedCurve = new int[50];
+	static double[] BellShapedCurve = new double[50];
 	static char[] Characters = {'a',4,'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',5,'s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X'};
 	static Random r = new Random();
 	static byte[] b = new byte[2];
@@ -12,16 +12,16 @@ public class Rand {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int c = 0;
+		double c = 0;
 		for (int x = 0; x < 25; x++) {
-			System.out.print(c);
+			c = Rand.BellCurve(x);
 			Rand.BellShapedCurve[x] = c;
-			c++;
+			System.out.println(c);
 		}
 		for (int x = 25; x < 50; x++) {
-			System.out.print(c);
+			c = Rand.BellCurve(x);
 			Rand.BellShapedCurve[x] = c;
-			c = c - 1;
+			System.out.println(c);
 		}
 
 		int a = 1000; //Change the size of this to change the frequency of each character, relative to the probability distribution
@@ -34,5 +34,16 @@ public class Rand {
 
 
 	}
-
+	/**
+	 * Generates points on a bell shaped curve distribution
+	 * @param x
+	 * @return
+	 */
+	public static double BellCurve(int x) {
+		double r = ((1 / (8 * (Math.sqrt((double) (2) * Math.PI)))) * (Math.pow(Math.E, ((Math.pow(x - 25, 2)) / (Math.pow(2 * 8, 2))))));
+		r = r * 100;
+		return r;
+		
+	}
+	
 }
