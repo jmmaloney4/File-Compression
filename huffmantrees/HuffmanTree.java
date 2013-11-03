b6cb57da196cf493b45f4abdea5284fe3011c5de
Jack Maloney <jmmaloney4@gmail.com>
2013-11-03 12:07:49 -0600

    adding comments to code

--- huffmantrees/HuffmanTree.java
+++ huffmantrees/HuffmanTree.java
@@ -19,7 +19,13 @@ import java.util.Scanner;
 public class HuffmanTree {
 
 	int a = 0;
-
+	
+	/**
+	 * takes the root node, a temporary string, and a table to put the codes and the character in
+	 * @param n
+	 * @param code
+	 * @param tbl
+	 */
 	public static void buildCodes(Node n, String code,
 			Map<Character, String> tbl) {
 		if (n.isLeaf()) {
@@ -36,6 +42,7 @@ public class HuffmanTree {
 	}
 
 	/**
+	 * the main function of the program
 	 * @param args
 	 * @throws IOException
 	 */
@@ -50,22 +57,25 @@ public class HuffmanTree {
 		long[] times = new long[a];
 
 		for (int s1 = 0; s1 < a; s1++) {
-
+			//time #1 for timing the program
 			long time1 = System.currentTimeMillis();
-
+			
 			Comparator NodeComparator = new NodeComparator();
 			Comparator NodeComparator2 = new NodeComparator2();
 			PriorityQueue<Node> priQue = new PriorityQueue(50, NodeComparator2);
 			PriorityQueue<Node> NodeQue = new PriorityQueue(50, NodeComparator);
+			//input file
 			File file = new File(args[0]);
 			Reader reader = new BufferedReader(new FileReader(file));
 			Reader reader2 = new BufferedReader(new FileReader(file));
 			Map<Character, Integer> table = new HashMap<Character, Integer>(40);
 			Map<Character, String> StrTable = new HashMap<Character, String>(40);
+			//output file, dosn't actually do anything right now, everything gets put to stdout
 			File writeFile = new File(args[1]);
 			Writer writer = new BufferedWriter(new FileWriter(writeFile, true));
 			Reader reader3 = new BufferedReader(new FileReader(writeFile));
-
+			
+			//reads the length of the file and records the frequency of each character
 			int c = reader.read();
 			int length = 0;
 			while (c != -1) {
@@ -87,7 +97,7 @@ public class HuffmanTree {
 			Node EOFNode = new Node();
 			EOFNode.setValue(-1);
 			priQue.add(EOFNode);
-
+			
 			while (iter.hasNext()) {
 				Entry<Character, Integer> set = iter.next();
 				Character ch = set.getKey();
@@ -130,7 +140,8 @@ public class HuffmanTree {
 			}
 			writer.append(StrTable.get(null));
 			writer.close();
-
+			
+			//second time for timeing the computation
 			long time2 = System.currentTimeMillis();
 
 			length = length * 8;
--- huffmantrees/Node.java
+++ huffmantrees/Node.java
@@ -1,7 +1,14 @@
 package huffmantrees;
-
+/**
+ * object for representing nodes on the tree
+ * @author jack
+ *
+ */
 public class Node {
-
+	
+	/**
+	 * constructor
+	 */
 	public Node() {
 		
 	}
@@ -14,39 +21,73 @@ public class Node {
 	private int num;
 	private boolean isRoot = false;
 	
+	/**
+	 * sets the right side child node
+	 */
 	public void setRightChild(Node node) {
 		this.rightChild = node;
 	}
-
+	
+	/**
+	 * sets the left side child node
+	 * @param node
+	 */
 	public void setLeftChild(Node node) {
 		this.leftChild = node;
 	}
-
+	
+	/**
+	 * sets the parent node
+	 * @param node
+	 */
 	public void setParent(Node node) {
 		this.parent = node;
 	}
-
+	
+	/**
+	 * returns the right child node
+	 * @return
+	 */
 	public Node getRightChild() {
 		return this.rightChild;
 	}
-
+	
+	/**
+	 * returns the left child node 
+	 * @return
+	 */
 	public Node getLeftChild() {
 		return this.leftChild;
 	}
-
+	
+	/**
+	 * returns the parent node
+	 * @return
+	 */
 	public Node getParent() {
 		return this.parent;
 	}
-
+	
+	/**
+	 * sets the node as root
+	 */
 	public void setRoot() {
 		this.parent = null;
 		this.isRoot = true;
 	}
-
+	
+	/**
+	 * returns the value for this node
+	 * @return
+	 */
 	public int getValue() {
 		return this.value;
 	}
-
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public Node addLeftToRight() {
 		if (this.getLeftChild() == null) {
 			return null;
@@ -60,41 +101,78 @@ public class Node {
 		}
 	}
 	
+	/**
+	 * sets the value of the node
+	 * @param val
+	 * @return
+	 */
 	public int setValue(int val) {
 		this.value = val;
 		return val;
 	}
 	
+	/**
+	 * sets the character if its a leaf node
+	 * @param cha
+	 */
 	public void setCharacter(Character cha) {
 		this.ch = cha;
 	}
 	
+	/**
+	 * gets the character, returns c, a Character, not a char
+	 * @return
+	 */
 	public Character getCharacter() {
 		return this.ch;
 	}
 	
+	/**
+	 * @param n
+	 * @return
+	 */
 	public int setNum(int n) {
 		this.num = n;
 		return n;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getNum() {
 		return this.num;
 	}
 	
+	/**
+	 * returns a boolean telling if this is a root node or not
+	 * @return
+	 */
 	public boolean isRoot() {
 		return this.isRoot;
 	}
 	
+	/**
+	 * returns a boolean telling if this is a leaf node or not 
+	 * @return
+	 */
 	public boolean isLeaf() {
 		return !(this.hasLeft() && this.hasRight());
 	}
 	
+	/**
+	 * tells if this node has a left child node
+	 * @return
+	 */
 	public boolean hasLeft() {
 		return (this.getLeftChild() != null);
 		
 	}
 	
+	/**
+	 * tells if this node has a right child node
+	 * @return
+	 */
 	public boolean hasRight() {
 		return (this.getRightChild() != null);
 	
